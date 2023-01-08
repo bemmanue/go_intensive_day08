bf0433bcfe51c8e2d2f6be311589e916bb6331a3
Burma Emmanuel <bemmanue@si-g4.kzn.21-school.ru>
2023-01-08 17:10:12 +0300

    small fix

--- ex02/chocolate.go
+++ ex02/chocolate.go
@@ -4,19 +4,7 @@ package main
 	#cgo CFLAGS: -x objective-c -DGL_SILENCE_DEPRECATION
 	#cgo LDFLAGS: -framework Cocoa -framework OpenGL
 	#include <OpenGL/gl3.h>
-	#import <Carbon/Carbon.h> // for HIToolbox/Events.h
 	#import <Cocoa/Cocoa.h>
-	#include <pthread.h>
-	#include <stdint.h>
-	#include <stdlib.h>
-	void startDriver();
-	void stopDriver();
-	void makeCurrentContext(uintptr_t ctx);
-	void flushContext(uintptr_t ctx);
-	uintptr_t doNewWindow(int width, int height, char* title);
-	void doShowWindow(uintptr_t id);
-	void doCloseWindow(uintptr_t id);
-	uint64_t threadID();
 	#include "application.h"
 	#include "window.h"
 */
